44f58875d213d353ac29ee8c8947828f13ea2d9b
Gabriela.Berdasco <gabriela_berdasco@hotmail.com>
2021-12-16 11:52:11 -0300

    Desafío clase 6

---
+++ src/componentes/main/ItemList.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+import Item from "./Item"
+
+const ItemList = ({productosPromesa}) => {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        productosPromesa().then((data) => {
+            setProductos(data);
+        })
+    }, []);
+
+
+    console.log(productos)
+    return(
+        <div className="contenedorCards">
+            {productos.map((item) => (
+             <Item key={item.id} nombre={item.nombre} descripción={item.descripción} precio={item.precio} stock={item.stock}/>   
+            )
+            )}
+        </div>
+    );
+}
+
+export default ItemList
--- src/componentes/main/ItemListContainer.js
+++ src/componentes/main/ItemListContainer.js
@@ -1,20 +1,39 @@
-import ItemCount from "./ItemCount";
+/* import ItemCount from "./ItemCount"; */
+import ItemList from "./ItemList";
 import "./styleMain.css"
+
+const productosPromesa = () => {
+    return new Promise((resolve) => {
+        setTimeout(
+            ()=>
+            resolve([
+                {id: "1", nombre: "Yerbera", descripción: "Yerbera con pico vertedor grande, flores fondo negro.", precio: 1116, stock: 2},
+                {id: "2", nombre: "Escurridor", descripción: "Escurridor de cubiertos, cepillos o pinceles.", precio: 552, stock: 7},
+                {id: "3", nombre: "Mate shine", descripción: "Mate shine glitter con asa, bombilla y sistema de vaciado.", precio: 506, stock: 5},
+                {id: "4", nombre: "Yerbera", descripción: "Yerbera con pico vertedor grande, modelo flores rosa.", precio: 1116, stock: 12},
+                {id: "5", nombre: "Set yerbera y azucarera", descripción: "Set yerbera y azucarera de hojalata, modelo terrazo.", precio: 1182, stock: 15},
+            ]),3000
+        )
+    })
+}
 
 const ItemListContainer = ({nombre, apellido}) => {
 
-    const onAdd = (contador, setContador) => {
+    /* const onAdd = (contador, setContador) => {
         if (contador !== 0) {
         setContador (1)
         }
-    }
+    } */
 
     return (
         <>
             <h2 className="itemListContainer">
             ¡Bienvenido a nuestra tienda virtual, {nombre} {apellido}!
             </h2>
-            <ItemCount stock={5} inicial={1} onAdd={onAdd}/>
+            <div>
+                <ItemList productosPromesa={productosPromesa}/>
+            </div>
+            {/* <ItemCount stock={5} inicial={1} onAdd={onAdd}/> */}
         </>
     );
 }
